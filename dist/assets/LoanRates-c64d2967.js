772acdbeea43118c9ad8670e1428c08448b19a3c
MaksimPyatak <108072564+MaksimPyatak@users.noreply.github.com>
2023-01-26 21:08:54 +0200

    adding dist

---
+++ dist/assets/LoanRates-c64d2967.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-b99ce810.js";const _={};function a(t,n){return" Loan Rates "}const o=e(_,[["render",a],["__scopeId","data-v-bee12af9"]]);export{o as default};
---
+++ dist/assets/TermsConditions-1e00ad61.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-b99ce810.js";const _={};function n(s,o){return" Terms Conditions "}const c=e(_,[["render",n],["__scopeId","data-v-18cdb65e"]]);export{c as default};
