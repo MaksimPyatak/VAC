55164dfb14455000a87983ff772a92dd8e5296ee
MaksimPyatak <108072564+MaksimPyatak@users.noreply.github.com>
2023-02-01 22:27:08 +0200

    Calculator

---
+++ assets/CatalogPage-d65e439a.js
@@ -0,0 +1,1 @@
+import{_}from"./index-a724e8bf.js";const a={};function e(t,o){return" Catalog "}const n=_(a,[["render",e],["__scopeId","data-v-32654a91"]]);export{n as default};
---
+++ assets/PrivacyPolicy-4cb57acf.js
@@ -0,0 +1,1 @@
+import{_}from"./index-a724e8bf.js";const c={};function e(a,o){return" Privacy Policy "}const t=_(c,[["render",e],["__scopeId","data-v-f8f1baa6"]]);export{t as default};
