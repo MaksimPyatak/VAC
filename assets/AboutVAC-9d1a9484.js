ab1f2940dd9c7d1a6858fa440ba51f1a2967ab3c
MaksimPyatak <108072564+MaksimPyatak@users.noreply.github.com>
2023-02-08 23:07:35 +0200

    adding dist

--- assets/AboutVAC-9d1a9484.js
+++
@@ -1,1 +0,0 @@
-import{_}from"./index-168000be.js";const e={};function t(o,c){return" AboutVAC "}const s=_(e,[["render",t],["__scopeId","data-v-987b1653"]]);export{s as default};
---
+++ assets/LoanRates-aaf0a6b5.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-3bd34780.js";const _={};function a(t,n){return" Loan Rates "}const o=e(_,[["render",a],["__scopeId","data-v-bee12af9"]]);export{o as default};
