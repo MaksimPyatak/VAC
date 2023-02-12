c2f2c6e8bffafaa457d143a9c5831f44681aa028
MaksimPyatak <108072564+MaksimPyatak@users.noreply.github.com>
2023-02-12 22:56:51 +0200

    adding dist

---
+++ dist/assets/CarPage-821ad621.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-d1a67418.js";const _={};function a(r,t){return" Car Page "}const n=e(_,[["render",a],["__scopeId","data-v-e2de48ef"]]);export{n as default};
--- dist/assets/CatalogPage-b7a718f8.js
+++
@@ -1,1 +0,0 @@
-import{_}from"./index-accb4da0.js";const a={};function e(t,o){return" Catalog "}const n=_(a,[["render",e],["__scopeId","data-v-32654a91"]]);export{n as default};
