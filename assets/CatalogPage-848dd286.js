39b46302e1c9fdb707c079e1a768be109b5f1709
MaksimPyatak <108072564+MaksimPyatak@users.noreply.github.com>
2023-02-12 22:56:51 +0200

    adding dist

---
+++ assets/CatalogPage-848dd286.js
@@ -0,0 +1,1 @@
+import{_}from"./index-d1a67418.js";const a={};function e(t,o){return" Catalog "}const n=_(a,[["render",e],["__scopeId","data-v-32654a91"]]);export{n as default};
--- assets/TermsConditions-60debc79.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index-accb4da0.js";const _={};function n(s,o){return" Terms Conditions "}const c=e(_,[["render",n],["__scopeId","data-v-18cdb65e"]]);export{c as default};
