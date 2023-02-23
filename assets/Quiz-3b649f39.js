96841a5524149b4f39d796f2defe50f22b048be7
MaksimPyatak <108072564+MaksimPyatak@users.noreply.github.com>
2023-02-23 22:00:19 +0200

    adding dist

--- assets/Quiz-3b649f39.js
+++
@@ -1,1 +0,0 @@
-import{_}from"./index-d1a67418.js";const e={};function t(a,c){return" Quiz "}const s=_(e,[["render",t],["__scopeId","data-v-0a9e4ab2"]]);export{s as default};
---
+++ assets/create-pagination-23186119.js
@@ -0,0 +1,1 @@
+function i(t,e){const a=Math.ceil(t.length/e);for(let n=1;n<a+1;n++)this.pagination.push(n)}export{i as c};
