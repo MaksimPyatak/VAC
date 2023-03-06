3bc533acfea03deb2aba841963fe8c3b184ade24
MaksimPyatak <108072564+MaksimPyatak@users.noreply.github.com>
2023-03-06 13:54:58 +0200

    adding dist

--- dist/assets/create-pagination-23186119.js
+++
@@ -1,1 +0,0 @@
-function i(t,e){const a=Math.ceil(t.length/e);for(let n=1;n<a+1;n++)this.pagination.push(n)}export{i as c};
---
+++ dist/assets/create-pagination-dc2351ef.js
@@ -0,0 +1,1 @@
+function e(n,t){this.pagination.splice(0,this.pagination.length);const a=Math.ceil(n.length/t);for(let i=1;i<a+1;i++)this.pagination.push(i)}export{e as c};
