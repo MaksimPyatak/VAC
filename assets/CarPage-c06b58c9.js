a6f2f709acd7ab054b65177afd206e6e0f7372a2
MaksimPyatak <108072564+MaksimPyatak@users.noreply.github.com>
2023-03-06 13:54:58 +0200

    adding dist

--- assets/CarPage-c06b58c9.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o,c as t}from"./index-9c30a57c.js";const c={};function r(a,s,n,_,p,f){return o(),t("div")}const i=e(c,[["render",r]]);export{i as default};
--- assets/Quiz-3b65fd31.js
+++
@@ -1,1 +0,0 @@
-import{_}from"./index-9c30a57c.js";const e={};function t(a,c){return" Quiz "}const s=_(e,[["render",t],["__scopeId","data-v-0a9e4ab2"]]);export{s as default};
