d6b4cc8223a99f4c176472e33e4c3b301648141a
MaksimPyatak <108072564+MaksimPyatak@users.noreply.github.com>
2023-04-06 16:57:31 +0300

    adding dist

--- dist/assets/Arrow-Bottom-e8303cc4.js
+++
@@ -1,1 +0,0 @@
-const s="/VAC/assets/Arrow-Bottom-c4e23ba6.svg";export{s as _};
---
+++ dist/assets/Articles-ccc0a602.js
@@ -0,0 +1,1 @@
+import{_}from"./index-ad026ccc.js";const c={};function e(t,s){return" Articles "}const d=_(c,[["render",e],["__scopeId","data-v-24cdd86c"]]);export{d as default};
