db2e151d180a68a90abe2d2801030c45830c4fe5
MaksimPyatak <108072564+MaksimPyatak@users.noreply.github.com>
2023-04-10 13:03:48 +0300

    adding dist

---
+++ dist/assets/Rectangle287-16d230e5.js
@@ -0,0 +1,1 @@
+const s="/VAC/assets/1-6a80e5fd.jpg",t="/VAC/assets/2-5a83eb99.jpg",a="/VAC/assets/Rectangle286-6d8bd9a5.jpg",e="/VAC/assets/Rectangle287-9dce6d9e.jpg";export{s as _,t as a,a as b,e as c};
